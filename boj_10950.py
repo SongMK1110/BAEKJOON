7d72fef6a5f198e202bf9b4146a6ca24fe96fd13
Minkyu Song <thdalsrb123@naver.com>
2022-07-18 18:58:57 +0900

    first commit

---
+++ boj_10950.py
@@ -0,0 +1,6 @@
+# A+B-3
+t = int(input())
+
+for i in range(t):
+    a, b = map(int, input().split())
+    print(a+b)
---
+++ boj_2739.py
@@ -0,0 +1,7 @@
+# 구구단
+n = int(input())
+result = 0
+
+for i in range(1, 10):
+    result = n*i
+    print(n,"*",i,"=",n*i)
