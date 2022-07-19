e8b7519e9f9862b70c198a15269d2cd7ccbaeca9
Minkyu Song <thdalsrb123@naver.com>
2022-07-19 21:53:12 +0900

    first commit

---
+++ 문자열/boj_1152.py
@@ -0,0 +1,4 @@
+# 단어의 개수
+s = input().split()
+
+print(len(s))
---
+++ 문자열/boj_5622.py
@@ -0,0 +1,10 @@
+# 다이얼
+dial = ['ABC', 'DEF', 'GHI', 'JKL', 'MNO', 'PQRS', 'TUV', 'WXYZ']
+a = input()
+t = 0
+for i in range(len(a)):
+    for j in dial:
+        if a[i] in j:
+            t += dial.index(j) + 3
+
+print(t)
