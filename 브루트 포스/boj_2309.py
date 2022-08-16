3f3208c45335586dba8f6572f739306bac945b87
Minkyu Song <thdalsrb123@naver.com>
2022-08-16 22:10:38 +0900

    first commit

---
+++ 브루트 포스/boj_2309.py
@@ -0,0 +1,14 @@
+# 일곱 난쟁이
+arr = []
+for i in range(9):
+    arr.append(int(input()))
+
+for i in arr:
+    for j in arr:
+        if sum(arr) - i - j == 100 and i != j:
+            arr.remove(i)
+            arr.remove(j)
+
+arr.sort()
+for i in arr:
+    print(i)
