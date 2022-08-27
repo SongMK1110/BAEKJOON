b8e3b7c78563d97c82033358f6f5f9010edc30b5
Minkyu Song <thdalsrb123@naver.com>
2022-08-27 21:26:53 +0900

    first commit

---
+++ Do it/탐색/boj_13023.py
@@ -0,0 +1,33 @@
+# 친구 관계 파악하기
+import sys
+sys.setrecursionlimit(10000)
+input = sys.stdin.readline
+n, m = map(int, input().split())
+a = [[] for i in range(n + 1)]
+visited = [False] * (n+1)
+arrive = False
+
+def DFS(now, depth):
+    global arrive
+    if depth == 5:
+        arrive = True
+        return
+    visited[now] = True
+    for i in a[now]:
+        if not visited[i]:
+            DFS(i, depth + 1)
+    visited[now] = False
+
+for _ in range(m):
+    s, e = map(int, input().split())
+    a[s].append(e)
+    a[e].append(s)
+
+for i in range(n):
+    DFS(i, 1)
+    if arrive:
+        break
+if arrive:
+    print(1)
+else:
+    print(0)
